6dfa5bd5d40b57ecab51b90ab42fcbe1bcf0544b
Ahmer Munaf <ahmermunaf@gmail.com>
2024-04-27 13:00:23 +0400

    merging

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import Liveness from './src/Liveness'
+// import Liveness from './src/Liveness'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 // registerRootComponent(App);
-export default Liveness
+export default App
